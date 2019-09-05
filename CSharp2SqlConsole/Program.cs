0117951166000cbacea59b523cf5f6a925ee7957
Justin Dudley <justindudley99@gmail.com>
2019-09-05 10:04:21 -0400

    Add project files.

---
+++ CSharp2SqlConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using CSharp2SqlLibrary;
+
+namespace CSharp2SqlConsole {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
